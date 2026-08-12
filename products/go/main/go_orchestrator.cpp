71823478b1438754e33183db114e7d0b5aac5256
samuelbles07 <samuelbles07@gmail.com>
2026-08-12 23:32:16 +0700

    fix(go): handle cold shutdown on boot

--- products/go/main/go_orchestrator.cpp
+++ products/go/main/go_orchestrator.cpp
@@ -284,7 +284,8 @@ void Orchestrator::init(WakeCause cause, const BootHandoff &handoff) {
 
   _latest_power =
       _svc.power_service.poll_bms(_first_measurement_done && !_raw_measures.pm_a.is_pm_25_valid());
-  if (_latest_power.ship_mode_request == ShipModeRequest::OverTemperature) {
+  if (_latest_power.ship_mode_request == ShipModeRequest::OverTemperature ||
+      _latest_power.ship_mode_request == ShipModeRequest::UnderTemperature) {
     shutdown(_latest_power.ship_mode_request);
     return;
   }
